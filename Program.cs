c2a05646317d672c4bb1ea8de1025e26e3f2714a
Rubiniuz <ruben.engelage@gmail.com>
2023-11-29 16:15:49 +0100

    Fix validation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 using System.Reflection;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SDS.Database;
+using SDS.Mediator.Extensions;
+using SDS.Mediator.Requests;
+using SDS.Mediator.Validation;
+using SDS.Requests.Articles;
 using SDS.Requests.Articles.Create;
 using SDS.Requests.Articles.Delete;
 using SDS.Requests.Articles.Update;
@@ -27,10 +32,19 @@ builder.Services.AddMvc(options =>
 
 // Add all the containers and generate the swagger files accordingly
 builder.Services.AddControllers();
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddFluentValidationClientsideAdapters();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add validation
+var mediatorAssemblies = new[]
+{
+    typeof(Program).Assembly, // This assembly
+};
+
+builder.Services.AddMediatR(options => options.RegisterServicesFromAssemblies(mediatorAssemblies));
+builder.Services.AddValidatorsFromAssemblies(mediatorAssemblies);
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+builder.Services.AddValidationExceptionHandling();
 
 // Proceed to build the app
 var app = builder.Build();
